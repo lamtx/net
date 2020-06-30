c21e99a3ebbec6d4c70a6b0bdd7e6bcb86723f54
Lam To <toxuanlam@gmail.com>
2020-06-30 10:59:40 +0700

    remove required and requires due to conflict

--- lib/src/field_required_exception.dart
+++ lib/src/field_required_exception.dart
@@ -6,17 +6,3 @@ class FieldRequiredException implements Exception {
   @override
   String toString() => "$field is required";
 }
-
-T requires<T>(T any, String fieldName) {
-  if (any == null) {
-    throw FieldRequiredException(fieldName);
-  }
-  return any;
-}
-
-///
-/// Short form for [?? (throw FieldRequiredException(fieldName))]
-/// Consider remove when [Never] type supported
-T required<T>(String fieldName) {
-  throw FieldRequiredException(fieldName);
-}
