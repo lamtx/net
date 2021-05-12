c62bf13ce96e8f6ec18ae2db30c3f9e5776e50e3
Lam To <toxuanlam@gmail.com>
2021-05-12 11:49:58 +0700

    add Body factory

--- lib/src/body.dart
+++ lib/src/body.dart
@@ -1,9 +1,29 @@
 import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:http/http.dart';
 
 abstract class Body {
+  factory Body(Uint8List content, ContentType contentType) = _Body;
+
   ContentType get contentType;
 
   Stream<List<int>> get content;
 
   int get length;
+}
+
+class _Body implements Body {
+  _Body(this.data, this.contentType);
+
+  final Uint8List data;
+
+  @override
+  Stream<List<int>> get content => ByteStream.fromBytes(data);
+
+  @override
+  final ContentType contentType;
+
+  @override
+  int get length => data.length;
 }
