85b4a402e439bc904e571197c333dd362fb9c144
Lam To <toxuanlam@gmail.com>
2024-12-13 15:14:04 +0700

    Marker for classes that are annotated by JsonDecodable

--- lib/src/to_json.dart
+++ lib/src/to_json.dart
@@ -1,4 +1,18 @@
+import "dart:convert" as convert;
+
+import "utilities.dart";
+
 /// Marker for classes that are annotated by JsonDecodable
 abstract interface class ToJson {
   Map<String, Object?> toJson();
+}
+
+extension SerializableJsonObject on ToJson {
+  String serializeAsJson() {
+    return convert.json.encode(toJson());
+  }
+
+  String serializeAsUrlEncoded() {
+    return toUrlEncoded(toJson());
+  }
 }
